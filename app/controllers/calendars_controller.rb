91b52cf9aa760c18081fafc2c7d8b0ca18da1f76
kazumaban <1310350007t@gmail.com>
2020-11-02 14:21:16 +0900

    Revert "Merge branch 'master' into second_training"
    This reverts commit dd2d03d231cecc7cb822895376658f96b08f2fc1, reversing
    changes made to b15766a95abd750a94173abbc30b39d60f1e7a9d.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getweek
+    getWeek
     @plan = Plan.new
   end
 
@@ -18,7 +18,7 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def getweek
+  def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -35,7 +35,6 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-
       wday_num = Date.today.wday + x
       #Date.today.wdayを利用して添字となる数値を得る
       #もしもwday_numが7以上であれば、7を引く
@@ -44,7 +43,6 @@ class CalendarsController < ApplicationController
       end
 
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] }
-
       @week_days.push(days)
     end
 
