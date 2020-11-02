3f8d12e5247217588637c27773b16b2392dac604
kazumaban <1310350007t@gmail.com>
2020-11-02 17:51:15 +0900

    Revert "Merge pull request #10 from kazumaban/second-training-修正"
    This reverts commit 96d7cf154f9b8c5c5ae0bfe239cccd96b31afe51, reversing
    changes made to c4ad59603a38f28d2500d4d79b6134daec74476b.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan: params[:plan_params])
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
@@ -34,17 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-
-      wday_num = Date.today.wday + x
-      #Date.today.wdayを利用して添字となる数値を得る
-      #もしもwday_numが7以上であれば、7を引く
-      if wday_num >= 7 
-        wday_num = wday_num - 7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] }
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
